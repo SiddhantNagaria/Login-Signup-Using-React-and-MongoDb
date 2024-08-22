0db7ecfa9ef944609dac18e91703bfb40bd10086
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-08-22 23:53:33 +0530

    desgined navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,11 +1,16 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logout from './Logout';
 const Navbar = () => {
   return (
     <>
         <AppBar>
             <Toolbar>
-                <Typography>Siddhant Nagaria</Typography>
+                <Typography variant='h4' sx={{flexGrow:1}}>Siddhant Nagaria</Typography>
+                <Button variant="contained" to='/login' component={Link}>Login</Button>
+                <Button variant="contained"to='/signup' component={Link}>Signup</Button>
+                <Logout/>
             </Toolbar>
         </AppBar>
     </>
