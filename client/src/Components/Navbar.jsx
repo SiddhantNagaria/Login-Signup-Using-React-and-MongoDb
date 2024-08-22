a879f961b6dfb8a83aeb3d1a59a854234d601e73
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-08-22 23:27:39 +0530

    created components files

---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import React from 'react';
+const Navbar = () => {
+  return (
+    <>
+        <AppBar>
+            <Toolbar>
+                <Typography>Siddhant Nagaria</Typography>
+            </Toolbar>
+        </AppBar>
+    </>
+  )
+}
+
+export default Navbar
